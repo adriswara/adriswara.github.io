bff1a7d10ea71885ecf969e48f52700e2a5c61fd
adriswara <chevellerz454@yahoo.com>
2017-10-29 14:17:40 +0700

    Added light trail effect image with parralax effect and changed the hashtag on the image

---
+++ javascripttest.js
@@ -0,0 +1,22 @@
+console.log("loaded");
+var x;
+x = 10;
+
+Number
+var numrand = 0;
+function randGen(){
+    numrand = Math.random();
+}
+
+String
+Boolean
+Array
+var arrNum=[];
+///
+for (i = 0; i < 100; i++) {
+    
+    arrNum[i] = numrand;
+    randGen();
+    
+}
+console.log(arrNum);
